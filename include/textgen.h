311a5ee37d8e6b1035726d570ed8228095357ccb
KuznetsovMikhail <KuznetsovMikhail>
2021-04-28 21:04:03 +0300

    initial

---
+++ include/textgen.h
@@ -0,0 +1,4 @@
+// Copyright 2021 Kuznetsov Mikhail
+#ifndef INCLUDE_TEXGEN_H_
+#define INCLUDE_TEXGEN_H_
+#endif  // INCLUDE_TEXGEN_H_
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+// Copyright 2021 Kuznetsov Mikhail
+
+int main() {
+  return 0;
+}
+
+
