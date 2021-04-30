99b7c90c83a9c028790d4fcaa612c9e1f9f94666
AlexeusC4ever <62253233+AlexeusC4ever@users.noreply.github.com>
2021-04-30 21:35:56 +0300

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -5,26 +5,26 @@
 
 
 int main() {
-	std::map<prefix, std::vector<std::string> > statetab;
-	const int NPREF = 2;
-	const int MAXGEN = 1000;
-	std::string fin = "text.txt";
-	statetab = makeStatetab(NPREF, MAXGEN, fin);
-	prefix startPref = { "There", "are" };
-	auto it = statetab.find(startPref);
-	std::cout << it->first.size() << "\n\n";
-	for (auto it : startPref) {
-		std::cout << it << " ";
-	}
-	for (int i = 0; i < MAXGEN; ++i) {
-		std::string somestring =
-			generateText(&statetab, &startPref);
-		if (somestring.length()) {
-			std::cout << somestring << " ";
-			startPref.erase(startPref.begin());
-			startPref.push_back(somestring);
-		}
-	}
-	std::cout << statetab.size() << std::endl;
-	
+    std::map<prefix, std::vector<std::string> > statetab;
+    const int NPREF = 2;
+    const int MAXGEN = 1000;
+    std::string fin = "text.txt";
+    statetab = makeStatetab(NPREF, MAXGEN, fin);
+    prefix startPref = { "There", "are" };
+    auto it = statetab.find(startPref);
+    std::cout << it->first.size() << "\n\n";
+    for (auto it : startPref) {
+        std::cout << it << " ";
+    }
+    for (int i = 0; i < MAXGEN; ++i) {
+        std::string somestring =
+            generateText(&statetab, &startPref);
+        if (somestring.length()) {
+            std::cout << somestring << " ";
+            startPref.erase(startPref.begin());
+            startPref.push_back(somestring);
+        }
+    }
+    std::cout << statetab.size() << std::endl;
+    
 }
