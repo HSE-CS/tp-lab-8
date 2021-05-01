69fe4cc0929fb3cea0ba34e543b64538bf019221
anyaschenikova <58432297+anyaschenikova@users.noreply.github.com>
2021-05-01 23:43:28 +0300

    shine bright like a diamond

--- test/tests.cpp
+++ test/tests.cpp
@@ -5,34 +5,34 @@
 #include "../include/textgen.h"
 
 TEST(textgen, test1) {
-  prefix pr;
-  EXPECT_EQ(true, pr.empty());
+  prefix pref;
+  EXPECT_EQ(true, pref.empty());
 }
 
 TEST(textgen, test2) {
-  prefix pr;
-  pr.push_back("test");
-  pr.push_back("testik");
-  EXPECT_EQ(2, pr.size());
+  prefix pref;
+  pref.push_back("test");
+  pref.push_back("testik");
+  EXPECT_EQ(2, pref.size());
 }
 
 TEST(textgen, test3) {
-  prefix pr;
-  pr.push_back("Hello");
-  pr.push_back("world");
-  EXPECT_EQ("Hello", pr.at(0));
+  prefix pref;
+  pref.push_back("Hello");
+  pref.push_back("world");
+  EXPECT_EQ("Hello", pref.at(0));
 }
 
 TEST(textgen, test4) {
-  prefix pr;
-  pr.push_back("test");
-  statetab[pr].push_back("number 4");
-  EXPECT_EQ(1, statetab[pr].size());
+  prefix pref;
+  pref.push_back("Hello");
+  statetab[pref].push_back("World");
+  EXPECT_EQ(1, statetab[pref].size());
 }
 
 TEST(textgen, test5) {
-  prefix pr;
-  pr.push_back("Hello");
-  statetab[pr].push_back("World");
-  EXPECT_EQ("World", statetab[pr][0]));
+  prefix pref;
+  pref.push_back("Hello");
+  statetab[pref].push_back("World");
+  EXPECT_EQ("World", statetab[pref][0]);
 }
