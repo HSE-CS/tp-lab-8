bbc1931496af0da3254121aef439b387f04ddfa4
NoName <ihackit595@gmail.com>
2021-05-02 18:35:39 +0300

    write class in header file

---
+++ include/textgen.h
@@ -0,0 +1,32 @@
+// Copyright 2021 Bogomazov Mikhail
+#ifndef INCLUDE_TEXTGEN_H_
+#define INCLUDE_TEXTGEN_H_
+#include <string>
+#include <deque>
+#include <vector>
+#include <map>
+#include <typeinfo>
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
+
+class GenerationOfText {
+ private:
+    const int NPREF = 2;
+    const int MAXGEN = 1000;
+    std::vector<std::string> words;
+    typedef std::deque<std::string> prefix;
+    std::map<prefix, std::vector<std::string>> stateTab;
+
+ public:
+    GenerationOfText(
+        std::map<prefix, std::vector<std::string>> val,
+        std::vector<std::string> word);
+    GenerationOfText();
+    void genTheTable(std::string text);
+    std::string genNewText();
+    std::string readFromFile(std::string filename);
+    void writeToFile(std::string answer);
+};
+#endif  //  INCLUDE_TEXTGEN_H_
