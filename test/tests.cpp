12f4427808b625dbe79fdb1df5ff68608356c415
Vladislav-Kanev <vokanev@edu.hse.ru>
2021-05-02 09:51:31 +0300

    more test changes

--- test/tests.cpp
+++ test/tests.cpp
@@ -10,13 +10,13 @@ TEST(TextGenerator, test1) {
 
 TEST(TextGenerator, test2) {
   prefix pr;
-  pr.push_back("test");
+  pr.push_back("test #2");
   EXPECT_EQ(1, pr.size());
 }
 
 TEST(TextGenerator, test3) {
   prefix pr;
-  pr.push_back("test");
+  pr.push_back("test #3");
   pr.push_back("number 3");
   pr.pop_front();
   EXPECT_EQ("number 3", pr.at(0));
@@ -24,14 +24,14 @@ TEST(TextGenerator, test3) {
 
 TEST(TextGenerator, test4) {
   prefix pr;
-  pr.push_back("test");
-  statetab[pr].push_back("number 5");
+  pr.push_back("test #4");
+  statetab[pr].push_back("number 4");
   EXPECT_EQ("number 5", statetab[pr][0]);
 }
 
 TEST(TextGenerator, test5) {
   prefix pr;
-  pr.push_back("test");
-  statetab[pr].push_back("number 4");
+  pr.push_back("test #5");
+  statetab[pr].push_back("number 5");
   EXPECT_EQ(1, statetab[pr].size());
 }
