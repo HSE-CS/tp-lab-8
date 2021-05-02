79861b70d4412cdbf72d94c9bb160903a00c1d13
NoName <ihackit595@gmail.com>
2021-05-02 18:41:05 +0300

    init blank cpp funcs

---
+++ src/textgen.cpp
@@ -0,0 +1,26 @@
+// Copyright 2021 Bogomazov Mikhail
+#include "../include/textgen.h"
+
+void GenerationOfText::genTable(std::string text) {
+    
+}
+
+std::string GenerationOfText::genText() {
+
+}
+std::string GenerationOfText::readFromFile(std::string filename) {
+    
+}
+
+void GenerationOfText::writeToFile(std::string answer) {
+    
+}
+
+GenerationOfText::GenerationOfText(
+    std::map<prefix, std::vector<std::string>> val,
+    std::vector<std::string> word) {
+}
+
+GenerationOfText::GenerationOfText() {
+
+}
