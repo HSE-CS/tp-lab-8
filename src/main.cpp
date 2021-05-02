1d9e75d7943fa0de0a63859209631d6c0cc7daf2
NoName <ihackit595@gmail.com>
2021-05-02 19:04:21 +0300

    added input data and wrote main

--- src/main.cpp
+++ src/main.cpp
@@ -2,5 +2,10 @@
 #include "../include/textgen.h";
 
 int main() {
+    GenerationOfText genText;
+    std::string text = genText.readFromFile("inp.txt");
+    genText.genTable(text);
+    std::string answer = genText.genText();
+    genText.writeToFile(answer);
     return 0;
 }
