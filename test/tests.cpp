23cd4d52684bec5eb9a40bcd46dbcdd447963b00
DenisKabanov <dskabanov@edu.hse.ru>
2021-05-04 11:31:52 +0300

    path 2.0

--- test/tests.cpp
+++ test/tests.cpp
@@ -4,14 +4,14 @@
 
 TEST(textgen, test1) {
   TextGen text1;
-  text1.setPath();
+  text1.setPath("src/input.txt");
   text1.ReadText();
   EXPECT_EQ(37, text1.getCount());
 }
 
 TEST(textgen, test2) {
   TextGen text2;
-  text2.setPath();
+  text2.setPath("src/input.txt");
   text2.ReadText();
   text2.makePairs();
   EXPECT_EQ("Nory was a Catholic because ", text2.createText(5));
