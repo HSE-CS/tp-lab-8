6e0c939fbc58bf6ffbeaa0fe7f73bd2467664a95
Kristina Egorova <kristinaeo2001@mail.ru>
2021-05-06 12:23:02 +0300

    First try

---
+++ include/textgen.h
@@ -0,0 +1,25 @@
+//  Copyright 2021 Kris
+
+
+#ifndef INCLUDE_TEXTGEN_H_
+#define INCLUDE_TEXTGEN_H_
+
+#include <deque>
+#include <vector>
+#include <map>
+#include <string>
+#include <fstream>
+
+typedef std::deque<std::string> prefix;
+std::map<prefix, std::vector<std::string> >
+makeStatetab(const int NPREF,
+    const int MAXGEN,
+    std::string filepath);
+
+std::string generateText(std::map<prefix,
+    std::vector<std::string> >* statetab,
+    prefix* startPref);
+
+std::string getLastWord(std::string str);
+
+#endif  // INCLUDE_TEXTGEN_H_ 
--- test/tests.cpp
+++ test/tests.cpp
@@ -1,2 +1,73 @@
-// Copyright 2021 GHA Test Team
+//  Copyright 2021 Kris
+
+
+#include "textgen.h"
 #include <gtest/gtest.h>
+#include <string>
+#include <iostream>
+
+TEST(Train, TEST1) {
+    std::map<prefix, std::vector<std::string> > statetab;
+    const int NPREF = 2;
+    const int MAXGEN = 1000;
+    std::string fin = "include/text.txt";
+    statetab = makeStatetab(NPREF, MAXGEN, fin);
+    prefix startPref = { "There", "are" };
+    auto it = statetab.find(startPref);
+    EXPECT_EQ(NPREF, it->first.size());
+}
+
+TEST(Train, TEST2) {
+    std::map<prefix, std::vector<std::string> > statetab;
+    const int NPREF = 2;
+    const int MAXGEN = 1000;
+    std::string fin = "include/text.txt";
+    statetab = makeStatetab(NPREF, MAXGEN, fin);
+    prefix startPref = { "There", "are" };
+    auto it = statetab.find(startPref);
+    std::string somestring = it->first[0];
+    somestring += ' ';
+    somestring += it->first[1];
+    somestring += ' ';
+    somestring += generateText(&statetab, &startPref);
+    EXPECT_EQ("There are a", somestring);
+}
+
+TEST(Train, TEST3) {
+    std::map<prefix, std::vector<std::string> > statetab;
+    const int NPREF = 2;
+    const int MAXGEN = 1000;
+    std::string fin = "include/text.txt";
+    statetab = makeStatetab(NPREF, MAXGEN, fin);
+    prefix startPref = { "There", "are" };
+    auto it = statetab.find(startPref);
+    std::string somestring = it->first[0];
+    somestring += ' ';
+    somestring += it->first[1];
+    somestring += ' ';
+    somestring += generateText(&statetab, &startPref);
+    EXPECT_EQ("There are a", somestring);
+}
+
+TEST(Train, TEST4) {
+    std::map<prefix, std::vector<std::string> > statetab;
+    prefix startPref = { "There", "are" };
+    std::vector<std::string> suffix;
+    suffix.push_back("first");
+    suffix.push_back("second");
+    statetab.emplace(startPref, suffix);
+    bool somestring2 = true;
+    std::string somestring = generateText(&statetab, &startPref);
+    EXPECT_EQ(true, somestring2);
+}
+
+TEST(Train, TEST5) {
+    std::map<prefix, std::vector<std::string> > statetab;
+    const int NPREF = 2;
+    const int MAXGEN = 1000;
+    std::string fin = "include/text.txt";
+    statetab = makeStatetab(NPREF, MAXGEN, fin);
+    prefix startPref = { "There", "are" };
+    auto it = statetab.find(startPref);
+    EXPECT_EQ(NPREF, it->first.size());
+}
