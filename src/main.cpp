b080a6d5d53dc87e1522892c85dd8289c29d3e7e
Vadukk <Vadik-Tumakov.com@yandex.ru>
2021-05-25 11:01:47 +0300

    2

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,9 @@
 // Copyright 2021 Vadukk
-
 #include "textgen.h"
 
 int main() {
     GenerationOfText test;
-    std::string text = test.ReadFromFile("Russia.txt");
+    std::string text = test.ReadFromFile("eminem.txt");
     test.MakingTheTable(text);
     std::string answer = test.GenerationNewText();
     test.WritingToFile(answer);
