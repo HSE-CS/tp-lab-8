9b40024404a4ffcdb0afb2f33fc7cd634f26ffb4
k-dash6 <dakozlova_1@edu.hse.ru>
2021-06-11 21:03:20 +0300

    adding tests

--- test/tests.cpp
+++ test/tests.cpp
@@ -14,3 +14,28 @@ TEST(Test, Test2) {
     pref.push_back("efgh");
     EXPECT_EQ(2, pref.size());
 }
+
+TEST(Test, Test3) {
+    prefix pref;
+    pref.push_back("abc");
+    statetab[pref].push_back("def");
+    statetab[pref].push_back("ghi");
+    statetab[pref].push_back("jkl");
+    EXPECT_EQ(3, statetab[pref].size());
+}
+
+TEST(Test, Test4) {
+    prefix pref;
+    pref.push_back("abc");
+    pref.push_back("def");
+    pref.push_back("ghi");
+    pref.pop_front();
+    EXPECT_EQ("ghi", pref.at(0));
+}
+
+TEST(Test, Test5) {
+    prefix pref;
+    pref.push_back("abc");
+    pref.push_back("def");
+    EXPECT_EQ("abc", pref.at(0));
+}
