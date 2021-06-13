9d4bf44ca3f4eca603781e91f81884e5f8c144a3
Lavrov-Artyom <artem3sandy@gmail.com>
2021-06-13 20:42:29 +0300

    third1.1

--- test/tests.cpp
+++ test/tests.cpp
@@ -5,14 +5,14 @@
 
 TEST(textgen, test1) {
 	TextGen text1;
-	text1.setPath("src/input.txt");
+	text1.setPath("src/input.TXT");
 	text1.ReadText();
 	EXPECT_EQ(73, text1.getCount());
 }
 
 TEST(textgen, test2) {
 	TextGen text2;
-	text2.setPath("src/input.txt");
+	text2.setPath("src/input.TXT");
 	text2.ReadText();
 	text2.makePairs();
 	EXPECT_EQ("Nory was a Catholic because ", text2.createText(5));
