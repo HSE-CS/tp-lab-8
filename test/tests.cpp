d2206854b64680e7ab3153215053eac0ab99daae
Polina1059 <polpan01@mail.ru>
2021-06-14 07:16:54 +0300

    tests

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,2 +1,36 @@
-// Copyright 2021 GHA Test Team
+// Copyright 2021 Panina Polina
 #include <gtest/gtest.h>
+
+#include "../include/textgen.h"
+
+TEST(textgen, test1) {
+  prefix pref;
+  EXPECT_EQ(true, pref.empty());
+}
+
+TEST(textgen, test2) {
+  prefix pref;
+  pref.push_back("abcd");
+  pref.push_back("abcdef");
+  EXPECT_EQ(2, pref.size());
+}
+
+TEST(textgen, test3) {
+  prefix pref;
+  pref.push_back("test");
+  pref.push_back("third");
+  EXPECT_EQ("test", pref.at(0));
+}
+
+TEST(textgen, test4) {
+  prefix pref;
+  pref.push_back("Hello");
+  statetab[pref].push_back("World");
+  EXPECT_EQ("World", statetab[pref][0]);
+}
+TEST(textgen, test5) {
+  prefix pref;
+  pref.push_back("Hello");
+  statetab[pref].push_back("World");
+  EXPECT_EQ(1, statetab[pref].size());
+}
